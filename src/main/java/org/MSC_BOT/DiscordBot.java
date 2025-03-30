5731f41d00d1cac1ac1ba0d395ebfd0de31bf989
MUNSC <msc825@naver.com>
2025-03-30 23:31:41 +0900

    테스트

--- src/main/java/org/MSC_BOT/DiscordBot.java
+++ src/main/java/org/MSC_BOT/DiscordBot.java
@@ -5,7 +5,7 @@ public class DiscordBot {
 
     public static void main(String[] args) {
         BotTokenManager tokenManager = new BotTokenManager();
-        String token = tokenManager.getDiscordBotToken( );
+        String token = tokenManager.getDiscordBotToken();
         JDABuilder.createDefault(token)
                 .build();
     }
